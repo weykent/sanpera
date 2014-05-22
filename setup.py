017c22f93a864c34b115bb9649d750bd9ab95ce0
weykent <weykent@weasyl.com>
2014-05-21 22:06:52 -0700

    Bump version again.
    The upload of the previous version failed.

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ def ext_module(module):
 
 setup(
     name='sanpera',
-    version='0.1.1-4',
+    version='0.1.1-5',
     description='Image manipulation library, powered by ImageMagick',
     author='Eevee',
     author_email='eevee.sanpera@veekun.com',
